83deca35ab538cf9630a8419419006c461036b39
sinancengiz <sinancengiz_84@hotmail.com>
2021-09-22 10:50:18 -0700

    user controller is craeted

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :games
-
+    
+    post 'signup', to: 'users#create'
     post 'auth/login', to: 'authentication#authenticate'
+
 end
