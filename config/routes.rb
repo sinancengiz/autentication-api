c3633d3649253dd99b74900abe0bd5d77b4c59bf
sinancengiz <sinancengiz_84@hotmail.com>
2021-09-22 10:48:52 -0700

    routes are updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     resources :games
+
+    post 'auth/login', to: 'authentication#authenticate'
 end
