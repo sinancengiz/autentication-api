69fd76ecb9d5384e4a1cea9bbda09540a6fb13e9
sinancengiz <sinancengiz_84@hotmail.com>
2021-09-22 16:12:54 -0700

    user information is returned with autenticate

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,9 +3,10 @@ class AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :authenticate
   # return auth token once user is authenticated
   def authenticate
+    user = User.find_by_email(auth_params[:email])
     auth_token =
       AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    json_response(user:user, auth_token: auth_token)
   end
 
   private
