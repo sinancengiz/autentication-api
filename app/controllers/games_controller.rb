d8b56d85083b5476634e24989d3b5fbb1f898621
sinancengiz <sinancengiz_84@hotmail.com>
2021-09-22 10:22:08 -0700

    crud methods are added to game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,43 @@
 class GamesController < ApplicationController
+    before_action :set_game, only: [:show, :update, :destroy]
+
+  # GET /games
+  def index
+    @games = Game.all
+    json_response(@games)
+  end
+
+  # POST /games
+  def create
+    @game = Game.create!(game_params)
+    json_response(@game, :created)
+  end
+
+  # GET /games/:id
+  def show
+    json_response(@game)
+  end
+
+  # PUT /games/:id
+  def update
+    @game.update(game_params)
+    head :no_content
+  end
+
+  # DELETE /games/:id
+  def destroy
+    @game.destroy
+    head :no_content
+  end
+
+  private
+
+  def game_params
+    # whitelist params
+    params.permit(:title, :created_by, :player_count)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
 end
