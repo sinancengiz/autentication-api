19ef8ad115f08e0fe25a0704e5a3a4602e565ff5
sinancengiz <sinancengiz_84@hotmail.com>
2021-10-21 11:39:17 -0700

    user name unique is added

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   has_and_belongs_to_many :games
   # Validations
   validates_presence_of :user_name, :email, :password_digest
-  validates_uniqueness_of :user_name
+  validates_uniqueness_of :user_name, email
 end
